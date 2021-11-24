b6d82b2f1427f01ef5b9fe80669caaaf1a91b88e
Besufikad <besufikadwondimu@gmail.com>
2021-11-24 12:50:25 +0500

    initial doc

---
+++ final_project.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+from newspaper import Article
+
+class Newspaper:
+
+    def __init__(self):
+        """this method will initialize the url attribute with the url of NYT"""
+
+
+    def sections(self):
+        """
+        this method will extract the sections from NYT website and ask the user to select the section and modify
+        the url attribute accordingly
+        return: None
+        """
+
+    def get_articles_section(self):
+        """this method will return the section of NYT
+                        return: section
+                        """
+
+    def get_articles_url(self):
+        """this method will return the url of list articles url
+                param: article object
+                return: articles_url
+                """
+
+    def get_article(self, link):
+        """ this method extracts all the articles from the section
+        return: list of articles
+        """
+
+
+def main():
+    """this method is the driver of the Newspaper class"""
+
+    
+if __name__ == "__main__":
+    main()
+
+    
