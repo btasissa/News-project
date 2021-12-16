12926beaf02efe7a2f3826a849c4aae89b9428a4
HGebrekiros <67560930+HRuth2020@users.noreply.github.com>
2021-12-17 02:24:02 +0500

    Update test.py

--- test.py
+++ test.py
@@ -16,3 +16,12 @@ class Test(unittest.TestCase):
    def test_get_article_url(self):
         self.assertEqual(self.newspaper.get_article_url(self.article),
                          "https://www.nytimes.com/2021/12/16/briefing/omicron-outbreaks-eu-russia-nato.html")
+        
+        
+    def test_get_published_date(self):
+        self.assertEqual(self.newspaper.get_publish_date(self.article),
+                         "12/16/2021")
+
+    def test_get_authors(self):
+        self.assertEqual(self.newspaper.get_authors(self.article),
+                         ['Melina Delkic'])
