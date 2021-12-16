082a6f9c450265f14c1b67af2b0237a8f7cb3417
Mahamud Mahamed <60480385+miVe75@users.noreply.github.com>
2021-12-17 02:25:21 +0500

    Update test.py

--- test.py
+++ test.py
@@ -25,3 +25,10 @@ class Test(unittest.TestCase):
     def test_get_authors(self):
         self.assertEqual(self.newspaper.get_authors(self.article),
                          ['Melina Delkic'])
+
+    def test_get_images(self):
+        self.assertEqual(self.newspaper.get_images(self.article),
+                         ['https://static01.nyt.com/images/2019/02/05/multimedia/author-melina-delkic/author-melina-delkic-thumbLarge.png', 'https://static01.nyt.com/images/2021/12/17/world/17ambriefing-asia-travel/merlin_199349271_4e28e594-a23f-4b6d-a3a7-6f7f0b26190b-articleLarge.jpg?quality=75&auto=webp&disable=upscale', 'https://static01.nyt.com/images/2021/12/17/world/121721ambriefing-asia-promo/121721ambriefing-asia-promo-facebookJumbo-v2.jpg', 'https://static01.nyt.com/images/2021/12/17/world/121721ambriefing-asia-promo/121721ambriefing-asia-promo-facebookJumbo-v2.jpg'])
+
+    def test_frequently_used_words(self):
+        self.assertEqual(self.newspaper.get_frequent_words(self.article, 3), ['france', 'omicron', 'britain'])
